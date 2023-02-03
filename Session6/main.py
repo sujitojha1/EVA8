4664a8a152d14e2b12fde7168a2913e973b5ea62
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-03 23:14:35 +0530

    minor changes

--- Session6/main.py
+++ Session6/main.py
@@ -77,7 +77,7 @@ class test:
         test_loss /= len(testloader.dataset)
         self.test_losses.append(test_loss)
 
-        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
             test_loss, correct, len(testloader.dataset),
             100. * correct / len(testloader.dataset)))
 
