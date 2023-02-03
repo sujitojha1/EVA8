207d26c9e6055a04b98a986cbf53ff2c730903ec
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-03 17:55:37 +0530

    minor changes

--- Session6/train.py
+++ Session6/train.py
@@ -21,7 +21,7 @@ def train(net, trainloader, device, criterion, optimizer, scheduler, EPOCHS):
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 400 == 399:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
