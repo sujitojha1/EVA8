5a08dcc4bb5692c2fbd8d3a60c7fa9c835ed4dbc
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-03 23:58:47 +0530

    monr changes

--- Session6/model.py
+++ Session6/model.py
@@ -79,9 +79,9 @@ class Net(nn.Module):
             nn.Conv2d(in_channels=6*64, out_channels=6*64, kernel_size=3, stride=1, padding=1, groups=6*64, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(6*64),
-            nn.Conv2d(in_channels=6*64, out_channels=128, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Conv2d(in_channels=6*64, out_channels=256, kernel_size=1, stride=1, padding=0, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(256),
             nn.Dropout(dropout_value)
         ) # output_size = 4 #o/p size = 128*4*4 RF = 52
         
@@ -90,7 +90,7 @@ class Net(nn.Module):
             nn.AvgPool2d(kernel_size=4)
         ) #o/p size = 512*1*1 RF = 92
 
-        self.linear = nn.Linear(128, 10)
+        self.linear = nn.Linear(256, 10)
         self.dropout = nn.Dropout(dropout_value)
 
     def forward(self, x):
